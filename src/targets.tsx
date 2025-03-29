f4f8d041082fd9e0c15d4d45a861e9546797238e
cpl121.eth <cesar.peonlam@gmail.com>
2025-03-29 21:11:27 +0100

    feat: moving targets

--- src/targets.tsx
+++ src/targets.tsx
@@ -20,6 +20,21 @@ export const Target = ({ targetIdx }: TargetProps) => {
     const target = useMemo(() => scene.clone(), []);
 
     useEffect(() => {
+        const interval = setInterval(() => {
+            targets.forEach((target) => {
+                target.rotation.y += 0.015;
+                target.rotation.x += 0.015;
+                target.rotation.z += 0.01;
+                target.position.x += Math.sin(target.rotation.y) * 0.015;
+                target.position.y += Math.sin(target.rotation.x) * 0.015;
+                target.position.z += Math.cos(target.rotation.y) * 0.01;
+            }
+            );
+        }, 1000 / 60);
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
         target.position.set(Math.random() * 10 - 5, targetIdx * 2 + 1, -Math.random() * 5 - 5);
         targets.add(target);
     }, []);
